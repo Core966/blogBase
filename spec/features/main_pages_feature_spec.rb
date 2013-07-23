e4975bb848613cfd604f1903af16ef5d5b86abaf
OpenShift guest <core966@indamail.hu>
2013-07-23 15:02:16 -0400

    Created additional tests (starting to get the hang of it finally) and added menu for navigation

--- spec/features/main_pages_feature_spec.rb
+++ spec/features/main_pages_feature_spec.rb
@@ -1,10 +1,30 @@
 require 'spec_helper'
 
 feature "site content" do
-  scenario "Getting the right content" do
+  scenario "Home site greets properly" do
     visit main_pages_home_path
     #page.should have_css?('title:contains("Welcome to blogBase!")')
     #page.has_content?('fake text')
-    expect(page).to have_text("Welcome to blogBase!")
+    #expect(page).to have_text("Welcome to blogBase!")
+    expect(page).to have_css('h1:contains("Welcome to blogBase!")')
   end
 end
+
+feature "navigating menu" do
+  scenario "User should land on blogs page when he clicks on the blogs link at home page" do
+    visit main_pages_home_path
+    click_link "blogs_link"
+    current_path.should == "/main_pages/blogs"
+  end
+  scenario "User should land on about page when he clicks on the about link at home page" do
+    visit main_pages_home_path
+    click_link "about_link"
+    current_path.should == "/main_pages/about"
+  end
+  scenario "User should land on the same page when he clicks on the home link at home page" do
+    visit main_pages_home_path
+    click_link "home_link"
+    current_path.should == "/main_pages/home"
+  end
+end
+    
--- spec/requests/main_pages_spec.rb
+++ spec/requests/main_pages_spec.rb
@@ -1,27 +1,29 @@
 require 'spec_helper'
 
 describe "MainPages" do
-  describe "GET /main_pages_home" do
-    it "works! (now write some real specs)" do
+    
+describe "GET /main_pages_home" do
+    it "should have valid route and should render template given" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get main_pages_home_path
       response.status.should be(200)
+      response.should render_template("layouts/application")
     end
-    
-
   end
 describe "GET /main_pages_blogs" do
-    it "works! (now write some real specs)" do
+    it "should have valid route and should render template given" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get main_pages_blogs_path
       response.status.should be(200)
+      response.should render_template("layouts/application")
     end
   end
 describe "GET /main_pages_about" do
-    it "works! (now write some real specs)" do
+    it "should have valid route and should render template given" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get main_pages_about_path
       response.status.should be(200)
+      response.should render_template("layouts/application")
     end
   end
 end
