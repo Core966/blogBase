90ac739aa8b37c7b2611aa0d02114bdffe012f91
OpenShift guest <core966@indamail.hu>
2013-07-24 16:39:41 -0400

    Edited the views and added Pure CSS elements,

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ BlogBase::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
